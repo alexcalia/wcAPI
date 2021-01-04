beab678d00b59f27bd4d3dfda4b8770640db861a
Alex Calia <alex.m.calia@outlook.com>
2021-01-04 15:41:42 -0500

    installed cors, need to troubleshoot post requests from web apps

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 // Import Routes
@@ -16,6 +17,7 @@ mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopol
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Route Middlewares
 app.use('/api/user', authRoute);
