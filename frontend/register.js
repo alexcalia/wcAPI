8d03610ab5afcb18c0f972a9849e9705ff317514
Alex Calia <alex.m.calia@outlook.com>
2021-01-05 10:22:42 -0500

    basic user registration is working

--- frontend/register.js
+++ frontend/register.js
@@ -8,14 +8,20 @@ register.password = document.getElementById('password');
 // Register submit
 
 register.submit = async (event) => {
-  event.preventDefault();
-  const response = await fetch('http://localhost:3000/api/user/register', {
-    method: 'POST',
-    body: {
+  const data = {
       name: register.name.value,
       email: register.email.value,
       password: register.password.value
     }
+
+  event.preventDefault();
+  const response = await fetch('http://localhost:3000/api/user/register', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
   })
 
   console.log(response)
