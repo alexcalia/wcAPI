bd58763a3f055e979ce3ba4845caa3ccc3f0ea26
Alex Calia <alex.m.calia@outlook.com>
2021-01-06 13:51:04 -0500

    successful console log of the jwt upon login

--- frontend/login.js
+++ frontend/login.js
@@ -4,23 +4,26 @@ const login = {}
 login.email = document.getElementById('email');
 login.password = document.getElementById('password');
 
-//Login submit
-
-login.submit = async (event) => {
-  const data = {
-    email: login.email.value,
-    password: login.password.value
-  }
-
-  event.preventDefault();
-  const response = await fetch('http://localhost:3000/api/user/login', {
+login.loginRequest = (data) => {
+  fetch('http://localhost:3000/api/user/login', {
     method: 'POST',
     mode: 'cors',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
-  })
+  }).then(response => response.text())
+  .then(response => console.log(response))
+}
 
-  console.log(response);
+//Login submit
+login.submit = (event) => {
+  const data = {
+    email: login.email.value,
+    password: login.password.value
+  }
+
+  event.preventDefault();
+
+  login.loginRequest(data);
 }
--- frontend/register.js
+++ frontend/register.js
@@ -7,13 +7,14 @@ register.password = document.getElementById('password');
 
 // Register submit
 register.submit = async (event) => {
+  event.preventDefault();
+  
   const data = {
       name: register.name.value,
       email: register.email.value,
       password: register.password.value
     }
 
-  event.preventDefault();
   const response = await fetch('http://localhost:3000/api/user/register', {
     method: 'POST',
     mode: 'cors',
@@ -23,5 +24,5 @@ register.submit = async (event) => {
     body: JSON.stringify(data)
   })
 
-  console.log(response)
+  console.log(response.json())
 }
