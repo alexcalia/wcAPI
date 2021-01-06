0716f051604781cbf619026cdd27e1c6a011b8d8
Alex Calia <alex.m.calia@outlook.com>
2021-01-06 14:41:18 -0500

    grabbed response from egister fetch, also added error response handling

--- frontend/register.js
+++ frontend/register.js
@@ -6,23 +6,33 @@ register.email = document.getElementById('email');
 register.password = document.getElementById('password');
 
 // Register submit
-register.submit = async (event) => {
-  event.preventDefault();
-  
-  const data = {
-      name: register.name.value,
-      email: register.email.value,
-      password: register.password.value
-    }
-
-  const response = await fetch('http://localhost:3000/api/user/register', {
+register.registerRequest = (data) => {
+  fetch('http://localhost:3000/api/user/register', {
     method: 'POST',
     mode: 'cors',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(data)
+  }).then(response => {
+    if (!response.ok) {
+      return response.text()
+    } else {
+      return response.json()
+    }
   })
+  .then(response => console.log(response))
+}
 
-  console.log(response.json())
+//Register submit
+register.submit = (event) => {
+  const data = {
+    name: register.name.value,
+    email: register.email.value,
+    password: register.password.value
+  }
+
+  event.preventDefault();
+
+  register.registerRequest(data);
 }
