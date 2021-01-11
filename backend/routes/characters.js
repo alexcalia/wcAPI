61678dc149b9274f4e8f155b0fb43e2800a32258
Alex Calia <alex.m.calia@outlook.com>
2021-01-11 15:45:20 -0500

    playing around with cursor setup, need to give IDs to each character

--- backend/routes/characters.js
+++ backend/routes/characters.js
@@ -10,32 +10,42 @@ router.get('/', verifyKey, async (req, res) => {
   const classId = req.query.class;
   const raceId = req.query.race;
   const limit = parseInt(req.query.limit);
-  const cursor = req.query.cursor
+  const cursor = req.query.cursor;
   let character;
   let payload;
 
   try {
     if(factionId && classId && raceId) {
       // Find by class, race and faction
-      character = await Character.find({$and: [{'faction.id': factionId}, {'class.id': classId}, {'race.id': raceId}]});
+      payload = await Character.find({$and: [{'faction.id': factionId}, {'class.id': classId}, {'race.id': raceId}]});
     } else if (factionId && classId) {
       // Find by faction and class
-      character = await Character.find({$and: [{'faction.id': factionId}, {'class.id': classId}]});
+      payload = await Character.find({$and: [{'faction.id': factionId}, {'class.id': classId}]});
     } else if (classId && raceId) {
       // Find by class and race
-      character = await Character.find({$and: [{'race.id': raceId}, {'class.id': classId}]});
+      payload = await Character.find({$and: [{'race.id': raceId}, {'class.id': classId}]});
     } else if (factionId && raceId) {
       // Find by faction and race
-      character = await Character.find({$and: [{'race.id': raceId}, {'faction.id': factionId}]});
+      payload = await Character.find({$and: [{'race.id': raceId}, {'faction.id': factionId}]});
     } else if (factionId || classId || raceId) {
       // Find by faction, or race, or class
-      character = await Character.find({$or: [{'faction.id': factionId}, {'class.id': classId}, {'race.id': raceId}]})
+      payload = await Character.find({$or: [{'faction.id': factionId}, {'class.id': classId}, {'race.id': raceId}]});
+    } else if (cursor) {
+      //Find all characters from cursor (pagination)
+      character = await Character.find({_id: { $gt: cursor}}).limit(limit ? limit : 4);
     } else {
       // Find all chaarcters
       character = await Character.find({}).limit(limit ? limit : 4);
+      console.log(character[0]._id)
+      payload = {
+        data: character, 
+        cursor: {
+          previous: character[0]._id,
+          next: character[character.length-1]._id
+        }}
     }
 
-    res.send(character);
+    res.send(payload);
   } catch (err) {
     res.status(400).send(err);
   }
